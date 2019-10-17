62ffe19678d3b9524321a5f150eb93236ef5b118
Jacey Davis <jaceyd923@gmail.com>
2019-10-17 17:17:52 -0600

    added js requirements for assignment

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+// INSERT HEADER TEXT
 document.querySelector('header > h1').innerText = "Inspector Clouseau"
 document.querySelector('header > h2').innerText = "This IZ Chief Inspector Clouseau speaking on the pheaun"
 
+// WINDOW PROPERTIES
 function reDisplay() {
     let windowSizes = `Window size is ${window.innerWidth}px wide by ${window.innerHeight}px tall`
     let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} from the top of the display`
+    let myURL = `The URL for this page is ${window.location}`
 
-    let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset}`
+    document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${myURL}`
 }
 reDisplay()
+
+// DOCUMENT PROPERTIES
+document.querySelector('#myDocument').innerText = `The document title for this page is ${document.title} \n This page was last modified on ${document.lastModified}`
 
