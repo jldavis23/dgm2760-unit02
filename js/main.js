21bc1cf205370dae0435dd9dce8438ab6fd1846a
Jacey Davis <jaceyd923@gmail.com>
2019-10-17 18:06:13 -0600

    page styling

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // INSERT HEADER TEXT
 document.querySelector('header > h1').innerText = "Inspector Clouseau"
-document.querySelector('header > h2').innerText = "This IZ Chief Inspector Clouseau speaking on the pheaun"
+document.querySelector('header > h2').innerText = '"This IZ Chief Inspector Clouseau speaking on the pheaun"'
 
 // WINDOW PROPERTIES
 function reDisplay() {
-    let windowSizes = `Window size is ${window.innerWidth}px wide by ${window.innerHeight}px tall`
-    let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} from the top of the display`
-    let myURL = `The URL for this page is ${window.location}`
+    let windowSizes = `WINDOW SIZE: ${window.innerWidth}px x ${window.innerHeight}px`
+    let offset = `WINDOW OFFSET: ${window.screenX} from the left, ${window.screenY} from the top`
+    let myURL = `URL: ${window.location}`
 
     document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${myURL}`
 }
 reDisplay()
 
 // DOCUMENT PROPERTIES
-document.querySelector('#myDocument').innerText = `The document title for this page is ${document.title} \n This page was last modified on ${document.lastModified}`
+document.querySelector('#myDocument').innerText = `DOCUMENT TITLE: ${document.title} \n LAST MODIFIED: ${document.lastModified}`
 
